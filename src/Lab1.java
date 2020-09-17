ba835e84dadfc2117e7beb5ed0c7e82dc7558c51
smitcs19 <smitcs19@wfu.edu>
2020-09-17 12:22:32 -0400

    Final Version

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,26 @@
 public class Lab1 {
 
     public static void main(String[] args) {
-        System.out.println("Fix me!");
+        //System.out.println("Hello World");
+
+        int n;
+
+        try{
+            n = Integer.parseInt(args[0]);
+            if(n==args.length-1) {
+                for (int i = 1; i <= n; i++) {
+                    System.out.println(args[i]);
+                }
+            } else {
+                System.out.println("There int value does not match the number of names given.");
+            }
+        }
+        catch (NumberFormatException e1) {
+            System.out.println("Int is not first");
+        }
+        catch (ArrayIndexOutOfBoundsException e2) {
+            System.out.println("The int is too large");
+        }
+
     }
 }
